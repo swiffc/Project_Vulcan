82038776414773210d64f4236bc497f49f80db22
David Cornealius <davidcornealius@gmail.com>
2025-12-20 21:12:02 -0600

    Auto-sync: 2025-12-20 21:12:02

--- apps/web/src/components/cad/index.ts
+++ apps/web/src/components/cad/index.ts
@@ -1,4 +1,6 @@
+export { Dashboard } from "./Dashboard";
 export { Projects } from "./Projects";
 export { PartsLibrary } from "./PartsLibrary";
+export { DesignTools } from "./DesignTools";
 export { ECNTracker } from "./ECNTracker";
 export { ExportDrive } from "./ExportDrive";
